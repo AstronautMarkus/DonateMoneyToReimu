af24473efc8109b1502c381777337d3694ae77f4
Marcus Sky <astronautmarkus@gmail.com>
2023-09-22 20:00:41 -0300

    Agregar archivos de proyecto.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnConsejo = New Button()
+        imgReimu = New PictureBox()
+        titleP1 = New Label()
+        btnDonate = New Button()
+        titleP2 = New Label()
+        Button1 = New Button()
+        CType(imgReimu, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnConsejo
+        ' 
+        btnConsejo.BackColor = Color.LemonChiffon
+        btnConsejo.Cursor = Cursors.Hand
+        btnConsejo.Font = New Font("Roboto", 26.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        btnConsejo.Location = New Point(184, 478)
+        btnConsejo.Name = "btnConsejo"
+        btnConsejo.Size = New Size(410, 70)
+        btnConsejo.TabIndex = 0
+        btnConsejo.Text = "Dame un consejo sabio"
+        btnConsejo.UseVisualStyleBackColor = False
+        ' 
+        ' imgReimu
+        ' 
+        imgReimu.Image = My.Resources.Resources.reimu
+        imgReimu.Location = New Point(91, 67)
+        imgReimu.Name = "imgReimu"
+        imgReimu.Size = New Size(415, 394)
+        imgReimu.TabIndex = 1
+        imgReimu.TabStop = False
+        ' 
+        ' titleP1
+        ' 
+        titleP1.AutoSize = True
+        titleP1.Font = New Font("Roboto", 36F, FontStyle.Bold, GraphicsUnit.Point)
+        titleP1.Location = New Point(149, 6)
+        titleP1.Name = "titleP1"
+        titleP1.Size = New Size(385, 58)
+        titleP1.TabIndex = 2
+        titleP1.Text = "Consejos Sabios"
+        ' 
+        ' btnDonate
+        ' 
+        btnDonate.BackColor = Color.SpringGreen
+        btnDonate.Cursor = Cursors.Hand
+        btnDonate.Font = New Font("Roboto", 26.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        btnDonate.Location = New Point(512, 67)
+        btnDonate.Name = "btnDonate"
+        btnDonate.Size = New Size(260, 100)
+        btnDonate.TabIndex = 3
+        btnDonate.Text = "Donar plata a Reimu"
+        btnDonate.UseVisualStyleBackColor = False
+        ' 
+        ' titleP2
+        ' 
+        titleP2.AutoSize = True
+        titleP2.Font = New Font("Roboto", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        titleP2.Location = New Point(524, 29)
+        titleP2.Name = "titleP2"
+        titleP2.Size = New Size(256, 29)
+        titleP2.TabIndex = 4
+        titleP2.Text = """Reimu Programming"""
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.SteelBlue
+        Button1.Cursor = Cursors.Hand
+        Button1.Font = New Font("Roboto", 26.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button1.Location = New Point(512, 173)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(260, 100)
+        Button1.TabIndex = 5
+        Button1.Text = "Coming soon"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        CausesValidation = False
+        ClientSize = New Size(780, 557)
+        Controls.Add(Button1)
+        Controls.Add(titleP2)
+        Controls.Add(btnDonate)
+        Controls.Add(titleP1)
+        Controls.Add(imgReimu)
+        Controls.Add(btnConsejo)
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MaximumSize = New Size(800, 600)
+        MinimizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Un poco de sabiduría de Reimu"
+        CType(imgReimu, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnConsejo As Button
+    Friend WithEvents imgReimu As PictureBox
+    Friend WithEvents titleP1 As Label
+    Friend WithEvents btnDonate As Button
+    Friend WithEvents titleP2 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+
+    Private githubClicks As Integer = 0 ' Variable de clicks para el secreto de la foto
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnConsejo.Click
+
+        ' Generar un número aleatorio entre 1 y 5
+        Dim random As New Random()
+
+        Dim numero As Integer = random.Next(1, 6)
+
+        ' Mostrar mensajes diferentes según el valor de la variable "numero"
+
+        Select Case numero
+            Case 1
+                MessageBox.Show("En el mundo de la informática, la paciencia es una virtud y la perseverancia es la clave del éxito.", "Consejo sabio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Case 2
+                MessageBox.Show("La seguridad informática es como un escudo invisible; nunca te das cuenta de su importancia hasta que la necesitas desesperadamente.", "Consejo sabio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Case 3
+                MessageBox.Show("La tecnología avanza a pasos agigantados, pero nunca olvides que la creatividad y la innovación son el motor que impulsa el progreso en la informática.", "Consejo sabio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Case 4
+                MessageBox.Show("En la era digital, la información es poder, pero solo aquellos que la gestionan de manera inteligente pueden convertirla en conocimiento valioso.", "Consejo sabio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Case 5
+                MessageBox.Show("La informática es un puente que conecta mundos. Asegúrate de construir puentes fuertes y seguros para un futuro digital sólido y confiable.", "Consejo sabio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Select
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles imgReimu.Click
+
+        Dim github_markus As String = "https://github.com/AstronautMarkus"
+        githubClicks += 1
+
+        Try
+
+            If githubClicks = 3 Then
+                Process.Start("cmd", "/c start " & github_markus)
+                githubClicks = 0
+            End If
+
+        Catch ex As Exception
+
+            ' Manejar la excepción aquí, por ejemplo, mostrar un mensaje de error.
+
+            MessageBox.Show("Error al abrir el enlace: " & ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnDonate.Click
+
+        Dim input As String = InputBox("¿Cuánto dinero quieres donar?", "Donación a Reimu")
+
+        If String.IsNullOrEmpty(input) Then
+            MessageBox.Show("No has donado nada :(", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ElseIf IsNumeric(input) AndAlso Val(input) > 0 Then
+            MessageBox.Show($"Has donado {input} a Reimu ;)", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Por favor, ingresa una cantidad válida mayor a 0.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+
+        MessageBox.Show("Proximamente los consejos serán cientos y por una base de datos", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        MessageBox.Show("¿Alguien está leyendo esto?", "¿Hola?", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+End Class
